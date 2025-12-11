f40d67cce5b5e071a4ed9bc2f6e9bbf985db3489
Agyin-Obeng <agyinobeng60@gmail.com>
2025-12-11 22:54:30 +0000

    integrate formik

--- form-handling-react/src/components/FormikForm.js
+++ form-handling-react/src/components/FormikForm.js
@@ -1,10 +1,12 @@
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
 
+// Validation schema with Yup
 const validationSchema = Yup.object({
   username: Yup.string().required("Username is required"),
-  email: Yup.string().email("Invalid email").required("Email is required"),
-  password: Yup.string().min(6, "Min 6 characters").required("Password is required")
+  email: Yup.string().email("Invalid email address").required("Email is required"),
+  password: Yup.string().min(6, "Password must be at least 6 characters")
+    .required("Password is required"),
 });
 
 function FormikForm() {
@@ -15,29 +17,54 @@ function FormikForm() {
       <Formik
         initialValues={{ username: "", email: "", password: "" }}
         validationSchema={validationSchema}
-        onSubmit={(values) => {
+        onSubmit={(values, { resetForm }) => {
           console.log("Formik Submitted:", values);
-          alert("User registered with Formik!");
+          alert("User registered successfully with Formik!");
+          resetForm();
         }}
       >
-        <Form>
-          <div>
-            <Field name="username" placeholder="Username" />
-            <ErrorMessage name="username" component="p" style={{ color: "red" }} />
-          </div>
+        {({ isSubmitting }) => (
+          <Form>
 
-          <div>
-            <Field name="email" placeholder="Email" />
-            <ErrorMessage name="email" component="p" style={{ color: "red" }} />
-          </div>
+            <div>
+              <Field name="username" placeholder="Username" />
+              <ErrorMessage
+                name="username"
+                component="p"
+                style={{ color: "red" }}
+              />
+            </div>
 
-          <div>
-            <Field type="password" name="password" placeholder="Password" />
-            <ErrorMessage name="password" component="p" style={{ color: "red" }} />
-          </div>
+            <br />
 
-          <button type="submit">Register</button>
-        </Form>
+            <div>
+              <Field name="email" placeholder="Email" />
+              <ErrorMessage
+                name="email"
+                component="p"
+                style={{ color: "red" }}
+              />
+            </div>
+
+            <br />
+
+            <div>
+              <Field type="password" name="password" placeholder="Password" />
+              <ErrorMessage
+                name="password"
+                component="p"
+                style={{ color: "red" }}
+              />
+            </div>
+
+            <br />
+
+            <button type="submit" disabled={isSubmitting}>
+              {isSubmitting ? "Submitting..." : "Register"}
+            </button>
+
+          </Form>
+        )}
       </Formik>
     </div>
   );
